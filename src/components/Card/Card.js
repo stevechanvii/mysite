bc6327fd8464809d3cc7c6f3bf3e55d016229aa9
Steve Chan <dche0007@student.monash.edu>
2020-06-03 23:33:16 +1000

    Design card for both front and back

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import MySite from "../../images/method-draw-image.svg"
+import GitHubIcon from "@material-ui/icons/GitHub"
 
 const Card = () => {
   return (
@@ -11,28 +12,50 @@ const Card = () => {
           <img src={MySite} alt="my site" className="card__picture--img" />
         </div>
         <h4 className="card__heading">
-          <span className="card__heading-span card__heading-span--1">
-            Steve's portfolio
-          </span>
+          <span className="card__heading-span">Web</span>
         </h4>
-        <div className="card__details">
-          <div className="card__details__title">My portfolio</div>
-          <div className="card__details__tech">
-            <p className="card__details__tech--tag">ReactJS</p>
-            <p className="card__details__tech--tag">GatsbyJS</p>
-            <p className="card__details__tech--tag">Sass</p>
+        <div className="card__info">
+          <div className="card__info__title">My portfolio</div>
+          <div className="card__info__tech">
+            <p className="card__info__tech--label">ReactJS</p>
+            <p className="card__info__tech--label">GatsbyJS</p>
+            <p className="card__info__tech--label">Sass</p>
+            <p className="card__info__tech--label">AWS</p>
           </div>
         </div>
       </div>
-      <div className="card__side card__side--back card__side--back-1">
-        <div className="card__cta">
-          <div className="card__price-box">
-            <p className="card__price-only">Only</p>
-            <p className="card__price-value">$297</p>
+      <div className="card__side card__side--back">
+        <div className="card__desc">
+          <div className="card__desc__details">
+            <ul className="card__desc__details-ul">
+              <li className="card__desc__details-ul-li">
+                A single-page application developed by React and Gatsby
+                framework.
+              </li>
+              <li className="card__desc__details-ul-li">
+                Sass is used for beautiful design and animation.
+              </li>
+              <li className="card__desc__details-ul-li">
+                Deployed in AWS Amplify.
+              </li>
+            </ul>
           </div>
-          <a href="#" className="btn btn--white">
-            Book now!
-          </a>
+          <div className="card__desc__btn">
+            <a
+              className="btn__effect btn__effect-left btn__effect-left-view"
+              href="#"
+              title="Learn More"
+            >
+              Demo
+            </a>
+            <a
+              className="btn__effect btn__effect-left btn__effect-left-git"
+              href="#"
+              title="Learn More"
+            >
+              GitHub
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,14 @@ import React from "react"
 const Footer = () => {
   return (
     <footer className="footer">
-      © {new Date().getFullYear()}, Developed by
+      © {new Date().getFullYear()} Developed by
       {` `}
-      <a href="https://www.gatsbyjs.org">Danyang Chen</a>
+      <a href="" class="a__underline">
+        <span>Danyang Chen</span>
+        <svg viewBox="0 0 13 20">
+          <polyline points="0.5 19.5 3 19.5 12.5 10 3 0.5" />
+        </svg>
+      </a>
     </footer>
   )
 }
