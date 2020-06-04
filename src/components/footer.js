0a7e065f77a7199034e7288bb0a29bb22846a957
Steve Chan <dche0007@student.monash.edu>
2020-06-04 20:47:31 +1000

    Finish projects page

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,8 +11,17 @@ const Footer = () => {
           <polyline points="0.5 19.5 3 19.5 12.5 10 3 0.5" />
         </svg>
       </a>
+      | Some icons from{" "}
+      <a
+        href="https://www.flaticon.com/authors/freepik"
+        className="a__underline"
+      >
+        <span>Freepik</span>
+        <svg viewBox="0 0 13 20">
+          <polyline points="0.5 19.5 3 19.5 12.5 10 3 0.5" />
+        </svg>
+      </a>
     </footer>
   )
 }
-
 export default Footer
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,23 @@
-import Portfolio from "../images/project-portfolio.svg"
+import resume from "../images/projects/resume.svg"
+import Portfolio from "../images/projects/portfolio.svg"
+import covid19 from "../images/projects/covid19.svg"
+import noMilk from "../images/projects/no-milk.svg"
+import whackMole from "../images/projects/whack-mole.svg"
+import burger from "../images/projects/burger.svg"
+import booking from "../images/projects/booking.svg"
+import house from "../images/projects/house.svg"
+import tour from "../images/projects/tour.svg"
+import search from "../images/projects/search.svg"
 
 /* Color palettes
  * https://coolors.co/f94144-f3722c-f8961e-f9c74f-90be6d-43aa8b-577590
  * 
  * Data formate
 {
+  title: "",
   backgroundStyle: {backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)"},
   image: Object,
   imageTitle: "",
-  title: "",
   labels: [{name: "React", color: "#4880EC"}],
   content: ["", "", ""],
   button: [{name: "Demo", link: "", style: {}, color: ""]
@@ -16,17 +25,17 @@ import Portfolio from "../images/project-portfolio.svg"
 */
 export default [
   {
+    title: "stevechan",
     backgroundStyle: {
       backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
     },
-    image: Portfolio,
+    image: resume,
     imageTitle: "Web",
-    title: "My Portfolio",
     labels: [
-      { name: "React", color: "#5FD5F5" },
-      { name: "Gatsby", color: "#663399" },
-      { name: "Sass", color: "#CF649A" },
-      { name: "AWS", color: "#23303E" },
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
     ],
     content: [
       "A single-page application developed by React and Gatsby framework.",
@@ -38,13 +47,310 @@ export default [
         name: "Demo",
         link: "stevechanvii.me",
         style: "btn__effect-left-view",
-        color: "#CF649A",
+        color: "#30559D",
       },
       {
         name: "GitHub",
         link: "https://github.com/stevechanvii/mysite",
         style: "btn__effect-left-git",
-        color: "#CF649A",
+        color: "#3C6BC5",
+      },
+    ],
+  },
+  {
+    title: "Covid-19 Tracker",
+    backgroundStyle: {
+      backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
+    },
+    image: covid19,
+    imageTitle: "Web",
+    labels: [
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
+    ],
+    content: [
+      "A single-page application developed by React and Gatsby framework.",
+      "Sass is used for beautiful design and animation.",
+      "Deployed in AWS Amplify.",
+    ],
+    button: [
+      {
+        name: "Demo",
+        link: "stevechanvii.me",
+        style: "btn__effect-left-view",
+        color: "#30559D",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/stevechanvii/mysite",
+        style: "btn__effect-left-git",
+        color: "#3C6BC5",
+      },
+    ],
+  },
+  {
+    title: "Poke Allergist",
+    backgroundStyle: {
+      backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
+    },
+    image: noMilk,
+    imageTitle: "App",
+    labels: [
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
+    ],
+    content: [
+      "A single-page application developed by React and Gatsby framework.",
+      "Sass is used for beautiful design and animation.",
+      "Deployed in AWS Amplify.",
+    ],
+    button: [
+      {
+        name: "Demo",
+        link: "stevechanvii.me",
+        style: "btn__effect-left-view",
+        color: "#30559D",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/stevechanvii/mysite",
+        style: "btn__effect-left-git",
+        color: "#3C6BC5",
+      },
+    ],
+  },
+  {
+    title: "Whack A Mole",
+    backgroundStyle: {
+      backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
+    },
+    image: whackMole,
+    imageTitle: "Game",
+    labels: [
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
+    ],
+    content: [
+      "A single-page application developed by React and Gatsby framework.",
+      "Sass is used for beautiful design and animation.",
+      "Deployed in AWS Amplify.",
+    ],
+    button: [
+      {
+        name: "Demo",
+        link: "stevechanvii.me",
+        style: "btn__effect-left-view",
+        color: "#30559D",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/stevechanvii/mysite",
+        style: "btn__effect-left-git",
+        color: "#3C6BC5",
+      },
+    ],
+  },
+  {
+    title: "Devloper Portfolio",
+    backgroundStyle: {
+      backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
+    },
+    image: Portfolio,
+    imageTitle: "Web",
+    labels: [
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
+    ],
+    content: [
+      "A single-page application developed by React and Gatsby framework.",
+      "Sass is used for beautiful design and animation.",
+      "Deployed in AWS Amplify.",
+    ],
+    button: [
+      {
+        name: "Demo",
+        link: "stevechanvii.me",
+        style: "btn__effect-left-view",
+        color: "#30559D",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/stevechanvii/mysite",
+        style: "btn__effect-left-git",
+        color: "#3C6BC5",
+      },
+    ],
+  },
+  {
+    title: "My Burger",
+    backgroundStyle: {
+      backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
+    },
+    image: burger,
+    imageTitle: "Web",
+    labels: [
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
+    ],
+    content: [
+      "A single-page application developed by React and Gatsby framework.",
+      "Sass is used for beautiful design and animation.",
+      "Deployed in AWS Amplify.",
+    ],
+    button: [
+      {
+        name: "Demo",
+        link: "stevechanvii.me",
+        style: "btn__effect-left-view",
+        color: "#30559D",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/stevechanvii/mysite",
+        style: "btn__effect-left-git",
+        color: "#3C6BC5",
+      },
+    ],
+  },
+  {
+    title: "Adventure Tours",
+    backgroundStyle: {
+      backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
+    },
+    image: tour,
+    imageTitle: "Web",
+    labels: [
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
+    ],
+    content: [
+      "A single-page application developed by React and Gatsby framework.",
+      "Sass is used for beautiful design and animation.",
+      "Deployed in AWS Amplify.",
+    ],
+    button: [
+      {
+        name: "Demo",
+        link: "stevechanvii.me",
+        style: "btn__effect-left-view",
+        color: "#30559D",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/stevechanvii/mysite",
+        style: "btn__effect-left-git",
+        color: "#3C6BC5",
+      },
+    ],
+  },
+  {
+    title: "Real Property",
+    backgroundStyle: {
+      backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
+    },
+    image: house,
+    imageTitle: "Web",
+    labels: [
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
+    ],
+    content: [
+      "A single-page application developed by React and Gatsby framework.",
+      "Sass is used for beautiful design and animation.",
+      "Deployed in AWS Amplify.",
+    ],
+    button: [
+      {
+        name: "Demo",
+        link: "stevechanvii.me",
+        style: "btn__effect-left-view",
+        color: "#30559D",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/stevechanvii/mysite",
+        style: "btn__effect-left-git",
+        color: "#3C6BC5",
+      },
+    ],
+  },
+  {
+    title: "Holiday Booking",
+    backgroundStyle: {
+      backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
+    },
+    image: booking,
+    imageTitle: "Web",
+    labels: [
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
+    ],
+    content: [
+      "A single-page application developed by React and Gatsby framework.",
+      "Sass is used for beautiful design and animation.",
+      "Deployed in AWS Amplify.",
+    ],
+    button: [
+      {
+        name: "Demo",
+        link: "stevechanvii.me",
+        style: "btn__effect-left-view",
+        color: "#30559D",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/stevechanvii/mysite",
+        style: "btn__effect-left-git",
+        color: "#3C6BC5",
+      },
+    ],
+  },
+  {
+    title: "Job Seeker",
+    backgroundStyle: {
+      backgroundImage: "linear-gradient(to right bottom, #4880EC, #019CAD)",
+    },
+    image: search,
+    imageTitle: "Web",
+    labels: [
+      { name: "React", color: "#07C8F9" },
+      { name: "Gatsby", color: "#8055AA" },
+      { name: "Sass", color: "#D77EAB" },
+      { name: "AWS", color: "#48525E" },
+    ],
+    content: [
+      "A single-page application developed by React and Gatsby framework.",
+      "Sass is used for beautiful design and animation.",
+      "Deployed in AWS Amplify.",
+    ],
+    button: [
+      {
+        name: "Demo",
+        link: "stevechanvii.me",
+        style: "btn__effect-left-view",
+        color: "#30559D",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/stevechanvii/mysite",
+        style: "btn__effect-left-git",
+        color: "#3C6BC5",
       },
     ],
   },
