fde46ccf3f09b31c8408e833176e1da715921844
stevechanvii <chendanyangvii@gmail.com>
2022-09-17 23:02:47 +1000

    change content

--- src/data/modal.js
+++ src/data/modal.js
@@ -1,8 +1,8 @@
 export default {
-  title: ", greeting from Danyang!",
+  title: ", greeting from Steve!",
   content: [
-    "Hi, I'm Danyang (Steve). Currently looking for frontend opportunities in Melbourne. As a graduate of Monash University (Master of IT), I have focused my career and academic goal on frontend development, especially React and React Native. Coding has made my life full of challenges and a sense of accomplishment, and I pride myself as a very effective learner and developer.",
-    "During this difficult period, I do not stop learning. Apart from my full-time job, I developed stevechanvii.me, and a developer portfolio website by Node.js. My next goal is to become a full-stack developer, and the good news is I just become an Amazon AWS Certified Associate Architect. However, I know I still have a long way to go, but I look forward to making my effort and applying my skills to your company.",
-    "I am passionate about my career as a programmer. If you are interested in my experiences and projects, please contact me.",
+    "Hi, I'm Danyang (Steve). Currently working at a startup Heidi Health. As a mid-level frontend developer, I have focused my career on website and cross platform mobile Apps using React and React Native. Coding has made my life full of challenges and a sense of accomplishment, and I pride myself as a very effective learner and developer.",
+    "My next goal is to become a full-stack developer, and the good news is I just become an Amazon AWS Certified Associate Architect. However, I know I still have a long way to go, but I look forward to making my effort and applying my skills to your company.",
+    "I am passionate about my career as a programmer. And I'm interested in photography, running.",
   ],
 }
